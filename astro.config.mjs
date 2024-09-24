97c1e1c9678e27c927bc1437c69e2692b02aeaaf
Naoki Komazawa <komazawa@ekzm.co.jp>
2024-09-24 17:16:51 +0900

    commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
+import react from '@astrojs/react'
+import compress from 'astro-compress'
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import react from '@astrojs/react';
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()]
-});
+  server: {
+    open: true,
+  },
+  devToolbar: {
+    enabled: false,
+  },
+  build: {
+    // format: 'file',
+    inlineStylesheets: 'never',
+  },
+  compressHTML: false,
+  integrations: [react(), compress({ CSS: false, Image: false, SVG: false })],
+})
